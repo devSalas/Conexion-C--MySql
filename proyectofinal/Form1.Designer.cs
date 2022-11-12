e8841381db6e77f067afc40eddb5f3872cca1c60
Esteban Salas Sulca <esalassulca@gmail.com>
2022-11-11 23:10:45 -0500

    My first commit
    Esto es una conexion entre c# y mi base de datos loca mysql

---
+++ proyectofinal/Form1.Designer.cs
@@ -0,0 +1,192 @@
+﻿namespace proyectofinal
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dbTabla = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.nombre = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.edad = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dev = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtEdad = new System.Windows.Forms.TextBox();
+            this.txtProfesion = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dbTabla)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dbTabla
+            // 
+            this.dbTabla.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dbTabla.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.nombre,
+            this.edad,
+            this.dev});
+            this.dbTabla.Location = new System.Drawing.Point(99, 232);
+            this.dbTabla.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.dbTabla.Name = "dbTabla";
+            this.dbTabla.Size = new System.Drawing.Size(498, 174);
+            this.dbTabla.TabIndex = 0;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "id";
+            this.Column1.Name = "Column1";
+            // 
+            // nombre
+            // 
+            this.nombre.HeaderText = "nombre";
+            this.nombre.Name = "nombre";
+            // 
+            // edad
+            // 
+            this.edad.HeaderText = "edad";
+            this.edad.Name = "edad";
+            // 
+            // dev
+            // 
+            this.dev.HeaderText = "dev";
+            this.dev.Name = "dev";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(630, 125);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(87, 34);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "insertar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Bernard MT Condensed", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(303, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(260, 31);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Crud Api con C# y MySql";
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(138, 141);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(100, 26);
+            this.txtNombre.TabIndex = 3;
+            // 
+            // txtEdad
+            // 
+            this.txtEdad.Location = new System.Drawing.Point(285, 141);
+            this.txtEdad.Name = "txtEdad";
+            this.txtEdad.Size = new System.Drawing.Size(100, 26);
+            this.txtEdad.TabIndex = 3;
+            // 
+            // txtProfesion
+            // 
+            this.txtProfesion.Location = new System.Drawing.Point(427, 141);
+            this.txtProfesion.Name = "txtProfesion";
+            this.txtProfesion.Size = new System.Drawing.Size(100, 26);
+            this.txtProfesion.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(153, 94);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 19);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "nombre";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(305, 94);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(40, 19);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "edad";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(423, 94);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(67, 19);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "profesion";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Teal;
+            this.ClientSize = new System.Drawing.Size(820, 478);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtProfesion);
+            this.Controls.Add(this.txtEdad);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dbTabla);
+            this.Font = new System.Drawing.Font("Bernard MT Condensed", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dbTabla)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dbTabla;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nombre;
+        private System.Windows.Forms.DataGridViewTextBoxColumn edad;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dev;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtEdad;
+        private System.Windows.Forms.TextBox txtProfesion;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ proyectofinal/Form1.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace proyectofinal
+{
+    public partial class Form1 : Form
+    {
+        MySqlConnection myCon;
+
+        public Form1()
+        {
+            InitializeComponent();
+            conectar();
+            llenarTabla();
+        }
+        private void conectar()
+        {
+
+            try
+            {
+                /*string server = "railway";
+                string database = "containers-us-west-104.railway.app";
+                string user = "root";
+                string password = "aqkqJ4pUuwU3zTXtG56C";
+                int port = 6125;
+                string cadenaConecxion = "server=" + server + ";database=" + database + ";Uid=" + user + ";pwd=" + password+"port="+port;
+                myCon= new MySqlConnection(cadenaConecxion);
+                myCon.Open();*/
+                string server = "localhost";
+                string database = "mysql-prueba";
+                string user = "root";
+                string password = "";
+               
+                string cadenaConecxion = "server=" + server + ";database=" + database + ";Uid=" + user + ";pwd=" + password;
+                myCon = new MySqlConnection(cadenaConecxion);
+                myCon.Open();
+
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show("algo malo añ conectar");
+            }
+
+        }
+        public void llenarTabla()
+        {
+            string query = "select id,nombre,edad,dev from usuario";
+            Console.WriteLine(query);
+            MySqlCommand comandoDB = new MySqlCommand(query, myCon);
+            comandoDB.CommandTimeout = 60;
+            MySqlDataReader reader;
+            try
+            {
+                reader = comandoDB.ExecuteReader();
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        int n = dbTabla.Rows.Add();
+                        dbTabla.Rows[n].Cells[0].Value = reader.GetString(0);
+                        dbTabla.Rows[n].Cells[1].Value = reader.GetString(1);
+                        dbTabla.Rows[n].Cells[2].Value = reader.GetString(2);
+                        dbTabla.Rows[n].Cells[3].Value = reader.GetString(3);
+                    }
+                }
+                else
+                {
+                    Console.Write("no hay trabajadores");
+                }
+                reader.Close();
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine("algo malo al llamar");
+            }
+
+        }
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string query = "";
+            string mensajeError = "";
+            if (txtNombre.Text == "" || txtEdad.Text == "" || txtProfesion.Text == "")
+            {
+                mensajeError = "hay un campo vacio, por favor llenar todos";
+            }
+
+            if (mensajeError == "")
+            {
+                query = "insert into usuario " +
+                    "(nombre,edad,dev) values " +
+                    "('" + txtNombre.Text + "' , '" + txtEdad.Text + "' , '" + txtProfesion.Text + "')";
+                MySqlCommand comando = new MySqlCommand(query, myCon);
+                comando.CommandTimeout = 60;
+                MySqlDataReader reader;
+
+                try
+                {
+                    reader = comando.ExecuteReader();
+                    reader.Close();
+
+                    dbTabla.Rows.Clear();
+                    dbTabla.Refresh();
+                    llenarTabla();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex);
+                }
+            }
+            else
+            {
+                MessageBox.Show(mensajeError);
+            }
+        }
+    }
+}
